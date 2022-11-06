2f179488f79f82487588196a2fbc2a370a985dc4
Nuth Vireak <timberr.4t@gmail.com>
2022-11-06 21:40:10 +0700

    Finished Lab4

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,3 +1,102 @@
+import java.util.Scanner;
+
 public class Matrix {
 
-}
+
+    // ----------------------------------------------------------------------------------
+    // Fields
+    //-----------------------------------------------------------------------------------
+
+    static Scanner input = new Scanner(System.in); // Scanner object to get user input
+
+    static int[][] result;
+
+
+    // ----------------------------------------------------------------------------------
+    // Methods
+    //-----------------------------------------------------------------------------------
+
+    /**
+     * Method to create matrix N rows and M columns
+     * @param row number of rows
+     * @param column number of columns
+     * @return matrix with N rows and M columns
+     */
+    public static int[][] createMatrix(int row, int column) {
+
+        int[][] matrix = new int[row][column]; // Initialize matrix with row and column size from parameters
+
+        for (int i = 0; i < row; i++) { // Loop through each row
+            for (int j = 0; j < column; j++) {  // Loop through each column
+                System.out.print("Enter element [" + i + "][" + j + "]: "); // Prompt user to enter element
+                matrix[i][j] = input.nextInt(); // Get user input for element
+            }
+        }
+
+        // Display each matrix after creation
+        System.out.println("Matrix----------------------");
+        SingleThread.displayEachMatrix(row, column, matrix);
+        System.out.println("----------------------------");
+
+        return matrix;
+    }
+
+    /**
+     * Method to perform matrix multiplication between two matrices
+     * @param matrix1 first matrix
+     * @param matrix2 second matrix
+     * @return result of matrix multiplication
+     */
+    public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+
+        int row1 = matrix1.length; // Get number of rows from matrix1
+        int column1 = matrix1[0].length; // Get number of columns from matrix1
+        int row2 = matrix2.length; // Get number of rows from matrix2
+        int column2 = matrix2[0].length; // Get number of columns from matrix2
+
+        // Use try-catch to catch exception if matrix multiplication is not possible
+        try {
+            if (column1 != row2) { // If number of columns in matrix1 is not equal to number of rows in matrix2
+                System.out.println();
+                System.out.println("------------------------------------ERROR-------------------------------------------------------");
+                throw new Exception("Invalid matrix size. Number of columns in matrix 1 must be equal to number of rows in matrix 2.");
+            }
+        // Catch exception if matrix multiplication is not possible
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+
+        result = new int[row1][column2]; // Initialize result matrix with row1 and column2 size
+
+        for (int i = 0; i < row1; i++) { // Loop through each row in matrix1
+            for (int j = 0; j < column2; j++) { // Loop through each column in matrix2
+                for (int k = 0; k < column1; k++) { // Loop through each column in matrix1
+                    result[i][j] += matrix1[i][k] * matrix2[k][j]; // Multiply each element in matrix1 with each element in matrix2
+                }
+            }
+        }
+
+        return result; // Return result of matrix multiplication
+    }
+
+    /**
+     * Method to generate random matrix N rows and M columns
+     * @param n number of rows
+     * @param m number of columns
+     * @return matrix with N rows and M columns
+     */
+    public static int[][] autoGenerateMatrix(int n, int m) {
+
+        int[][] matrix = new int[n][m]; // Initialize matrix with row and column size from parameters
+
+        for (int i = 0; i < n; i++) { // Loop through each row
+            for (int j = 0; j < m; j++) { // Loop through each column
+                matrix[i][j] = (int) (Math.random() * 2); // Generate random number between 0 and 1
+            }
+        }
+
+        return matrix;
+    }
+
+} // End of Matrix class
---
+++ src/SingleThread.java
@@ -0,0 +1,181 @@
+import java.util.Scanner;
+
+public class SingleThread {
+
+
+    // ----------------------------------------------------------------------------------
+    // Fields
+    //-----------------------------------------------------------------------------------
+
+    static Scanner input = new Scanner(System.in);
+
+    static int row1 = 0;
+    static int column1 = 0;
+    static int row2 = 0;
+    static int column2 = 0;
+    static int[][] matrix1;
+    static int[][] matrix2;
+    static int[][] result;
+
+
+    // ----------------------------------------------------------------------------------
+    // Methods
+    //-----------------------------------------------------------------------------------
+
+    /**
+     * This method is used to display the menu
+     */
+    public static void createMenu() {
+
+        System.out.println();
+        System.out.println("MENU------------------------");
+        System.out.println("1. Create matrix");
+        System.out.println("2. Auto Generate matrix");
+        System.out.println("3. Multiply matrix");
+        System.out.println("4. Clear matrix");
+        System.out.println("5. Exit");
+        System.out.println("----------------------------");
+    }
+
+    /**
+     * This method is used to display each matrix
+     * @param row number of rows
+     * @param column number of columns
+     * @param matrix matrix to be displayed
+     */
+    public static void displayEachMatrix(int row, int column, int[][] matrix) {
+
+        for (int i = 0; i < row; i++) { // Loop through each row
+            System.out.print("| "); // Print left border
+            for (int j = 0; j < column; j++) { // Loop through each column
+                System.out.print(matrix[i][j] + " "); // Print element
+            }
+
+            System.out.print("|"); // Print right border
+            System.out.println(); // Print new line
+        }
+    }
+
+    /**
+     * This method is used to display Matrix 1 and Matrix 2
+     * @param matrix1 first matrix
+     * @param matrix2 second matrix
+     */
+    public static void displayAllMatrix(int[][] matrix1, int[][] matrix2) {
+
+        System.out.println();
+        System.out.println("Matrix * Matrix-------------");
+        displayEachMatrix(matrix1.length, matrix1[0].length, matrix1); // Display matrix1
+        System.out.println("*");
+        displayEachMatrix(matrix2.length, matrix2[0].length, matrix2); // Display matrix2
+        System.out.println("----------------------------");
+    }
+
+    /**
+     * This method is used to handle the menu selection, call the appropriate method, and display the result
+     */
+    public static void menuSelection() {
+
+        while (true) { // Loop until user selects option 5 to exit
+
+            createMenu(); // call createMenu method to display menu
+
+            System.out.print("Enter your choice: "); // Prompt user to enter choice
+            int choice = input.nextInt(); // Get user input for choice
+
+            switch (choice) { // Switch statement to handle menu selection
+
+                // Create matrix
+                case 1 -> {
+
+                    System.out.println();
+                    System.out.println("Matrix 01-------------------");
+                    System.out.print("Enter number of rows: "); // Prompt user to enter number of rows
+                    row1 = input.nextInt(); // Get user input for number of rows
+
+                    System.out.print("Enter number of columns: "); // Prompt user to enter number of columns
+                    column1 = input.nextInt(); // Get user input for number of columns
+
+                    System.out.println();
+
+                    matrix1 = Matrix.createMatrix(row1, column1); // Call createMatrix method to create matrix1
+
+                    System.out.println();
+                    System.out.println("Matrix 02-------------------");
+                    System.out.print("Enter number of rows: "); // Prompt user to enter number of rows
+                    row2 = input.nextInt(); // Get user input for number of rows
+
+                    System.out.print("Enter number of columns: "); // Prompt user to enter number of columns
+                    column2 = input.nextInt(); // Get user input for number of columns
+
+                    System.out.println();
+
+                    matrix2 = Matrix.createMatrix(row2, column2); // Call createMatrix method to create matrix2
+
+                    displayAllMatrix(matrix1, matrix2); // Call displayAllMatrix method to display matrix1 and matrix2
+                }
+
+                // Generate matrix
+                case 2 -> {
+
+                    System.out.println();
+                    System.out.println("Matrix 01-------------------");
+                    System.out.print("Enter number of rows: "); // Prompt user to enter number of rows
+                    row1 = input.nextInt(); // Get user input for number of rows
+
+                    System.out.print("Enter number of columns: "); // Prompt user to enter number of columns
+                    column1 = input.nextInt(); // Get user input for number of columns
+
+                    matrix1 = Matrix.autoGenerateMatrix(row1, column1); // Call autoGenerateMatrix method to create matrix1
+
+                    System.out.println();
+                    System.out.println("Matrix 02-------------------");
+                    System.out.print("Enter number of rows: "); // Prompt user to enter number of rows
+                    row2 = input.nextInt(); // Get user input for number of rows
+
+                    System.out.print("Enter number of columns: "); // Prompt user to enter number of columns
+                    column2 = input.nextInt(); // Get user input for number of columns
+
+                    matrix2 = Matrix.autoGenerateMatrix(row2, column2); // Call autoGenerateMatrix method to create matrix2
+
+                    displayAllMatrix(matrix1, matrix2); // Call displayAllMatrix method to display matrix1 and matrix2
+                }
+
+                // Multiply matrix
+                case 3 -> {
+
+                    if (matrix1 == null || matrix2 == null) { // Check if matrix1 or matrix2 is null
+                        System.out.println();
+                        System.out.println("Please create matrix first");
+
+                    } else { // If matrix1 and matrix2 is not null then proceed to multiply matrix
+
+                        result = Matrix.multiply(matrix1, matrix2); // Call multiply method to multiply matrix1 and matrix2
+
+                        System.out.println();
+                        System.out.println();
+                        System.out.println("RESULT----------------------");
+                        displayEachMatrix(result.length, result[0].length, result); // Call displayEachMatrix method to display result
+                        System.out.println("----------------------------");
+                        System.out.println();
+                    }
+                }
+
+                // Clear matrix
+                case 4 -> {
+
+                    matrix1 = null;
+                    matrix2 = null;
+                    result = null;
+
+                    System.out.println();
+                    System.out.println("Matrix cleared");
+                }
+
+                // Exit
+                case 5 -> System.exit(0);
+            }
+
+        } // end while loop
+    } // End of menuSelection()
+} // End of SingleThread.java
